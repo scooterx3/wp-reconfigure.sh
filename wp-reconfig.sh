4ef6090462c951bef40c1e01be1da4e5d519b386
Riley Loader <riley.loader@gmail.com>
2014-05-15 14:45:51 -0600

    fixed #1 and refined a bunch of stuff

--- wp-reconfig.sh
+++ wp-reconfig.sh
@@ -1,60 +1,60 @@
 #!/bin/bash
 echo "dude welcome"
-OPTIND=1
-backupfile='wp-config_original_.php'
+
 
 wpcfg() {
 
-
+	OPTIND=1
+	backupfile='wp-config_original_.php'
+	
 	if [[ $1 == '-h' ]]; then
 		echo "Now vomiting helpful information..."
 		return
 	fi
 
 	# I want verbosity OFF and auto mode by default (even if no arguments are specified)
-	verbose=false
-	mode='auto'
-	random_string=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 5 | head -n 1)
-	cpuser=`whoami`
+	
 
-	dbhost_new=''
-	dbname_new=''
-	dbpass_new=''
-	dbuser_new=''
+	unset dbhost_new dbname_new dbpass_new dbuser_new verbose
 
 	while getopts ":amcvh" opt; do
 		case $opt in
 			a)
-				mode='auto';
-				;;
+				mode='auto' ;;
 			m)
-				mode='manual';
-				;;
+				mode='manual' ;;
 			c) 
-				mode='current';
-				;;
+				mode='current' ;;
 			v)
-				verbose='true';
-				;;
+				verbose='true' ;;
 		esac
 	done
 	shift $((OPTIND-1))
 	OPTIND=1
 
-	
-	if [[ -f wp-config.php ]]; then
-		cp -n wp-config.php $backupfile
-	else
+	if [[ ! -f wp-config.php ]]; then
+		
 		echo "No wp-config.php!"
 		return
 	fi
 
-	#cp wp-config.php wp-config_whatevahbackup_.php
+	cp -n wp-config.php $backupfile
 	read -r dbhost_old dbname_old dbpass_old dbuser_old <<< $( egrep "^[^/].*['\"]DB_(NAME|USER|PASSWORD|HOST[^_])" wp-config.php | sort -d | sed "s/.*[\"']\(.*\)[\"'].*;.*/\1/" )
+
+	if [[ $mode == 'current' ]]; then
+#CURRENT
+		echo "$dbname_old $dbuser_old $dbpass_old"
+		return
+	fi
+
+	random_string=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 5 | head -n 1)
+	cpuser=`whoami`
+
 
 	if [[ $mode == 'manual' ]]; then
 #MANUAL
 		if [[ $verbose == 'true' ]]; then echo -e "\n[MANUAL]"; fi
+		#echo -e "db user [pass] [host]"
 		read -p "db user [pass] [host]" dbname_new dbuser_new dbpass_new dbhost_new
 		if [[ $verbose == 'true' ]]; then echo -e "You entered: \nDB: $dbname_new \nUser: $dbuser_new \nPass: $dbpass_new \nHost: $dbhost_new"; fi
 
@@ -70,15 +70,6 @@ wpcfg() {
 			dbhost_new='localhost'
 		fi
 
-	elif [[ $mode == 'current' ]]; then
-#CURRENT
-		verbose='false';
-		dbhost_new=''
-		dbname_new=''
-		dbpass_new=''
-		dbuser_new=''
-		echo "$dbname_old $dbuser_old $dbpass_old"
-
 	else
 #AUTO
 		if [[ $verbose == 'true' ]]; then echo -e "\n[AUTO]"; fi
@@ -87,7 +78,7 @@ wpcfg() {
 		# fix dbname
 		# if cPanel user is the same, keep it otherwise prefix whatever is there with cpuser_
 		if [[ $dbname_old == $cpuser"_"* ]]; then
-			if [[ $verbose == 'true' ]]; then echo "cPanel user identical, keeping it for db user"; fi
+			if [[ $verbose == 'true' ]]; then echo "cPanel user identical, keeping it for db name"; fi
 			dbname_new=$dbname_old
 		else
 			dbname_new=$cpuser"_"$dbname_old
@@ -120,20 +111,23 @@ wpcfg() {
 		fi
 		# host will always be 'localhost'
 		dbhost_new='localhost'
-		
-
 	fi
 
-	
 	if [[ -z $dbuser_new ]]; then
 		if [[ $verbose == 'true' ]]; then echo "dbuser_new was empty, exiting"; fi
 	
 	else
-		# The magic happens (replacing things)
-		sed -i -e "s|DB_NAME\(['\"]\),\s*\?\(['\"]\)${dbname_old}|DB_NAME\1, \2${dbname_new}|
-		s|DB_USER\(['\"]\),\s*\?\(['\"]\)${dbuser_old}|DB_USER\1, \2${dbuser_new}|
-		s|DB_PASSWORD\(['\"]\),\s*\?\(['\"]\)${dbpass_old}|DB_PASSWORD\1, \2${dbpass_new}|
-		s|DB_HOST\(['\"]\),\s*\?\(['\"]\)${dbhost_old}|DB_HOST\1, \2${dbhost_new}|" wp-config.php
+		# clean up right side for sed's use
+		for i in dbname_new dbuser_new dbpass_new dbhost_new; do
+			declare safe_$i=$(printf '%s' "${!i}" | sed -e 's|\\|\\\\|g; s|&|\\\&|g')
+		done
+
+		sed -i -e "s|DB_NAME\(['\"]\),\s*\(['\"]\).*\?\2|DB_NAME\1, \2$safe_dbname_new\2|
+		s|DB_USER\(['\"]\),\s*\(['\"]\).*\?\2|DB_USER\1, \2$safe_dbuser_new\2|
+		s|DB_PASSWORD\(['\"]\),\s*\(['\"]\).*\?\2|DB_PASSWORD\1, \2$safe_dbpass_new\2|
+		s|DB_HOST\(['\"]\),\s*\(['\"]\).*\?\2|DB_HOST\1, \2$safe_dbhost_new\2|" wp-config.php
+
+		# spit it out!
 		echo -e "\nOLD:\n   $dbname_old $dbuser_old $dbpass_old $dbhost_old"
 		echo -e "\nNEW:\n   $dbname_new $dbuser_new $dbpass_new $dbhost_new"
 		echo -e "\n"
