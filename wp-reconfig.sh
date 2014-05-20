0ec31b7e55a1d3ecd65ebab45fdf0f08037a8e07
Riley <riley.loader@gmail.com>
2014-05-20 11:05:27 -0600

    fixed #7

--- wp-reconfig.sh
+++ wp-reconfig.sh
@@ -46,9 +46,9 @@ HERE
 	fi
 
 	# I want auto mode by default (even if no arguments are specified)
-	unset dbhost_new dbname_new dbpass_new dbuser_new mode
+	unset dbhost_new dbname_new dbpass_new dbuser_new mode debug dbname_old_temp dbuser_old_temp
 
-	while getopts ":amcvh" opt; do
+	while getopts ":amcdh" opt; do
 		case $opt in
 			a)
 				mode='auto' ;;
@@ -56,6 +56,8 @@ HERE
 				mode='manual' ;;
 			c) 
 				mode='current' ;;
+			d)
+				debug='true' ;;
 		esac
 	done
 	shift $((OPTIND-1))
@@ -69,6 +71,12 @@ HERE
 
 	cp -n wp-config.php $backupfile
 	read -r dbhost_old dbname_old dbpass_old dbuser_old <<< $( egrep "^[^/].*['\"]DB_(NAME|USER|PASSWORD|HOST[^_])" wp-config.php | sort -d | sed "s/.*[\"']\(.*\)[\"'].*;.*/\1/" )
+	
+	# setting them to temp variables in case I run into default values down the road
+	dbname_old_temp=$dbname_old
+	dbuser_old_temp=$dbuser_old
+
+	random_name_string=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 5 | head -n 1)
 
 	if [[ $mode == 'current' ]]; then
 #CURRENT
@@ -76,7 +84,7 @@ HERE
 		return
 	fi
 
-	random_string=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 5 | head -n 1)
+	random_pw_string=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 5 | head -n 1)
 	cpuser=`whoami`
 
 	if [[ $mode == 'manual' ]]; then
@@ -88,7 +96,7 @@ HERE
 		# if no dbpass was entered, set it to a random string
 		if [[ -z $dbpass_new ]]; then
 			echo "No password given, assigning a random value..."
-			dbpass_new=$random_string
+			dbpass_new=$random_pw_string
 		fi
 
 		# If the 4th argument isn't passed (dbhost_new), just set it to 'localhost'
@@ -105,12 +113,23 @@ HERE
 		leftside_length=${#leftside}
 
 	#DBNAME
+		
+		# if it's a default value, set it to something random
+		if [[ $dbname_old == 'database_name_here' ]]; then
+			dbname_old=$random_name_string
+		fi
 		dbname_rightside=${dbname_old#$leftside}
+		if [[ $debug == 'true' ]]; then echo "dbname_rightside: $dbname_rightside"; fi
 		dbname_new=$leftside$dbname_rightside
 
 	#USERNAME	
 		
+		# if it's a default value, set it to something random
+		if [[ $dbuser_old == 'username_here' ]]; then
+			dbuser_old=$random_name_string
+		fi
 		dbuser_rightside=${dbuser_old#$leftside}
+		if [[ $debug == 'true' ]]; then echo "dbuser_rightside: $dbuser_rightside"; fi
 		if (( ${#dbuser_rightside} > 16-$leftside_length )); then
 			dbuser_new=$leftside${dbuser_rightside:$leftside_length-16}
 		else
@@ -118,18 +137,27 @@ HERE
 		fi
 
 	#PASSWORD
-		# pass is different, just keep it unless it's shorter than 5. If so, add a random 5 to the end of the string. 
-		if (( ${#dbpass_old} < 5 )); then 
-			echo "dbpass_old < 5 chars, adding random string: '$random_string'"
-			
-			dbpass_new=$dbpass_old$random_string #random_string defined at the top, I use it in more than 1 place
+
+		# if it's a default value, set it to something random
+		if [[ $dbpass_old == 'password_here' ]]; then 
+			echo "dbpass_old was default value, changing to random string: '$random_pw_string'"
+		
+		# If it's too short, add some characters to the end
+		elif (( ${#dbpass_old} < 5 )); then 
+			echo "dbpass_old < 5 chars, adding random string: '$random_pw_string'"
+			dbpass_new=$dbpass_old$random_pw_string #random_pw_string defined at the top, I use it in more than 1 place
+		
+		# otherwise, keep it
 		else 
 			echo "dbpass_old >= 5 chars, keeping old password"
 			dbpass_new=$dbpass_old
 		fi
+	
 	#HOST
+		
 		# host will always be 'localhost'
 		dbhost_new='localhost'
+	
 	fi
 
 	if [[ -z $dbuser_new ]]; then
@@ -146,7 +174,11 @@ HERE
 		s|DB_PASSWORD\(['\"]\),\s*\(['\"]\).*\?\2|DB_PASSWORD\1, \2$safe_dbpass_new\2|
 		s|DB_HOST\(['\"]\),\s*\(['\"]\).*\?\2|DB_HOST\1, \2$safe_dbhost_new\2|" wp-config.php
 
-		# spit it out!
+		# Set these values back to what the temp ones were. This shouldn't change anything except if the old values were default 'username_here' ones. 
+		dbname_old=$dbname_old_temp
+		dbuser_old=$dbuser_old_temp
+		
+		# spit out the info
 		if [[ $dbhost_new == 'localhost' ]]; then dbhost_new=''; echo "omitting 'localhost' from output of new credentials..."; fi
 		echo -e "\nOLD:\n   $dbname_old $dbuser_old $dbpass_old $dbhost_old"
 		echo -e "\nNEW:\n   $dbname_new $dbuser_new $dbpass_new $dbhost_new"
